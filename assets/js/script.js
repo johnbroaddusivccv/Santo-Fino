3df81118cfbf427d6f06d5222a8c7526b7e3a826
johnbroaddusivccv <johnbroaddusivccv@gmail.com>
2019-05-17 23:10:59 -0500

    responsive nav responsive owl

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,13 @@
 function openSlideMenu(){
     document.getElementById('side-menu').style.width = '250px';
     document.getElementById('main').style.marginLeft = '250px';
+    $('path').hide();
+
 }
 function closeSlideMenu(){
     document.getElementById('side-menu').style.width = '0';
     document.getElementById('main').style.marginLeft = '0';
+    $('path').show();
 }
 
 
